6b8ccaff84435467020d0c6566c33903acb5df0d
Shannon Wynter <me@example.com>
2017-05-11 22:58:35 +1000

    Log line 1

---
+++ eventloghook_test.go
@@ -0,0 +1,25 @@
+// +build windows
+
+package eventloghook
+
+import (
+	"testing"
+
+	"github.com/Sirupsen/logrus"
+	"golang.org/x/sys/windows/svc/debug"
+)
+
+func TestDebugLogAndPrint(t *testing.T) {
+	log := logrus.New()
+	elog = debug.New("ServiceName")
+	defer elog.Close()
+	log.Hooks.Add(NewEventLogHook(elog))
+
+	for _, level := range hook.Levels() {
+		if len(log.Hooks[level]) != 1 {
+			t.Errorf("EventLogHook was not added. The length of log.Hooks[%v]: %v", level, len(log.Hooks[level]))
+		}
+	}
+
+	log.Info("Congratulations!")
+}
